e7e7a8b389dc9356cda8c159da9a7714e63715a7
hranto <hei.ranto.2@gmail.com>
2025-09-18 22:09:05 +0300

    fix: change to read only the date in expense

--- src/components/ExpensesForm.tsx
+++ src/components/ExpensesForm.tsx
@@ -178,6 +178,7 @@ export default function ExpenseForm({
           <input
             type="date"
             value={dateDepense}
+            readOnly
             onChange={(e) => setDateDepense(e.target.value)}
             className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-amber-500 focus:border-transparent transition-colors"
             required
